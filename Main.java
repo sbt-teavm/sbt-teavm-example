76025e6b026c827eedc9cbb952a615e7000d0023
xuwei-k <6b656e6a69@gmail.com>
2023-11-09 22:50:02 +0900

    a

--- Main.java
+++ Main.java
@@ -5,7 +5,43 @@ import java.math.BigInteger;
 
 public class Main {
 
+  private static final float OTHER_NAN = Float.intBitsToFloat(Float.floatToIntBits(Float.NaN) + 1);
+
+
   public static void main(String[] args) {
+    testFloat();
+  }
+
+  static void testFloat() {
+    assertEquals(23, Float.parseFloat("23"), 1E-12F);
+    assertEquals(23, Float.parseFloat("23.0"), 1E-12F);
+    assertEquals(23, Float.parseFloat("23E0"), 1E-12F);
+    assertEquals(23, Float.parseFloat("2.30000E1"), 1E-12F);
+    assertEquals(23, Float.parseFloat("0.23E2"), 1E-12F);
+    assertEquals(23, Float.parseFloat("0.000023E6"), 1E-12F);
+    assertEquals(23, Float.parseFloat("00230000e-4"), 1E-12F);
+    assertEquals(23, Float.parseFloat("2300000000000000000000e-20"), 1E-12F);
+    assertEquals(23, Float.parseFloat("2300000000000000000000e-20"), 1E-12F);
+    assertEquals(23, Float.parseFloat("2300000000000000000000e-20"), 1E-12F);
+    assertEquals(23, Float.parseFloat("23."), 1E-12F);
+    assertEquals(0.1F, Float.parseFloat("0.1"), 0.001F);
+    assertEquals(0.1F, Float.parseFloat(".1"), 0.001F);
+    assertEquals(0.1F, Float.parseFloat(" .1"), 0.001F);
+    assertEquals(0.1F, Float.parseFloat(".1 "), 0.001F);
+    assertEquals(-23, Float.parseFloat("-23"), 1E-12F);
+    assertEquals(0, Float.parseFloat("0.0"), 1E-12F);
+    assertEquals(0, Float.parseFloat("0"), 1E-12F);
+    assertEquals(0, Float.parseFloat("00"), 1E-12F);
+    assertEquals(0, Float.parseFloat(".0"), 1E-12F);
+    assertEquals(0, Float.parseFloat("0."), 1E-12F);
+    assertEquals(0, Float.parseFloat("23E-8000"), 1E-12F);
+    assertEquals(0, Float.parseFloat("00000"), 1E-12F);
+    assertEquals(0, Float.parseFloat("00000.0000"), 1E-12F);
+    assertEquals(4499999285F, Float.parseFloat("4499999285"), 100F);
+    assertEquals(0.4499999285F, Float.parseFloat("0.4499999285"), 1E-9F);
+  }
+
+  static void testBigDecimal() {
     String a = "1231212478987482988429808779810457634781384756794987";
     int aScale = -15;
     String b = "747233429293018787918347987234564568";
@@ -19,7 +55,7 @@ public class Main {
     assertEquals("incorrect scale", cScale, result.scale());
   }
 
-  static void assertEquals(String c, Object x1, Object x2) {
+  static void assertEquals(Object c, Object x1, Object x2) {
     if(x1.equals(x2)) {
       System.out.println(x1.toString() + " == " + x2.toString() + " " + c);
     } else {
